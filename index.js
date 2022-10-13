41bc48fcba3b7572b47a868dd95651592835f6b9
susboi3 <115138582+susboi3@users.noreply.github.com>
2022-10-13 17:09:36 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const yearEl = document.getElementById("year");
+const monthEl = document.getElementById("month");
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let y = new Date().getFullYear();
+  let mo = new Date().getMonth();
+  let d = new Date().getDay();
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  y = y < 10 ? "0" + y : y;
+  mo = mo < 10 ? "0" + mo : mo;
+  d = d < 10 ? "0" + d : d;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  yearEl.innerText = y;
+  monthEl.innerText = mo;
+  dayEl.innerText = d;
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
